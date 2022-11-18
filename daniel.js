ed8ef67e783fa5a4dfe8d4f900b0d61f82adcf60
Dtheran <dtheran1@gmail.com>
2022-11-18 10:54:49 -0500

    implement effect, track, trigger

--- daniel.js
+++ daniel.js
@@ -1,21 +1,42 @@
 class DanielReactive {
+    deps = new Map();
     constructor(options) {
         this.origen = options.data();
+
+        const self = this;
         // Destino
         this.$data = new Proxy(this.origen, {
             get(target, name) {
                 if (Reflect.has(target, name)) {
+                    self.track(target, name)
                     return Reflect.get(target, name)
                 }
                 console.log('la Propiedad', name, 'no existe');
                 return ''
             },
             set(target, name, value) {
-                console.log('Modificando');
                 Reflect.set(target, name, value)
+                self.trigger(name)
             }
         })
+
     }
+
+    track(target, name) {
+        if (!this.deps.has(name)) {
+            const effect = () => {
+                document.querySelectorAll(`*[d-text=${name}]`).forEach(el => {
+                    this.dText(el, target, name)
+                })
+            }
+            this.deps.set(name, effect)
+        }
+    };
+
+    trigger(name) {
+        const effect = this.deps.get(name);
+        effect();
+    };
 
     mount() {
         document.querySelectorAll('*[d-text]').forEach(el => {
